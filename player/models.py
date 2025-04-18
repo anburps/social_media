0291ac3bd852fcaddfd7ddc7900d4ffcff28b4f8
anburps <anbalaganrps@gmail.com>
2025-04-18 12:08:26 +0530

    model created

--- player/models.py
+++ player/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    audio_file = models.FileField(upload_to='songs/')
+
+    def __str__(self):
+        return f"{self.title} - {self.artist}"
