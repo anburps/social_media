e41f6e20d83ed22096f23a668dd9dc0bfba8cc90
anburps <anbalaganrps@gmail.com>
2025-04-18 12:12:31 +0530

    template create and intgrated

--- player/views.py
+++ player/views.py
@@ -4,3 +4,6 @@ from .models import Song
 def song_list(request):
     songs = Song.objects.all()
     return render(request, 'player/song_list.html', {'songs': songs})
+
+def song_upload(request):
+    
